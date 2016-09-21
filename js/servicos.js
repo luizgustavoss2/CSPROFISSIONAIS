a7e501efc4918cc79de864322fa0faa9ce3034e0
Luiz Gustavo <luizgustavoss@hotmail.com>
2016-09-20 23:53:40 -0300

    alteracoes gustavo

--- js/servicos.js
+++ js/servicos.js
@@ -3399,3 +3399,17 @@ function closeMeNow() {
         });
     }
 }
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    document.addEventListener("backbutton", function (e) {
+        if ($.mobile.activePage.is('#homepage')) {
+            e.preventDefault();
+            navigator.app.exitApp();
+        }
+        else {
+            navigator.app.backHistory()
+        }
+    }, false);
+}
