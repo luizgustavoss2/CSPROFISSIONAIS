fd09d87a0d2d8819f2fcc93355105eebefa4496e
Luiz Gustavo <luizgustavoss@hotmail.com>
2016-09-21 01:01:11 -0300

    alteracoes gustavo

--- js/servicos.js
+++ js/servicos.js
@@ -3430,3 +3430,25 @@ function onConfirmQuit(button) {
         navigator.app.exitApp();
     }
 }
+
+document.addEventListener("backbutton", function () {
+    if ($('.ui-page-active').attr('id') == 'main') {
+        exitAppPopup();
+    } else {
+        history.back();
+    }
+}, false);
+
+function exitAppPopup() {
+    navigator.notification.confirm(
+          'Exit PhoneGap ' + device.cordova + ' Demo?'
+        , function (button) {
+            if (button == 2) {
+                navigator.app.exitApp();
+            }
+        }
+        , 'Exit'
+        , 'No,Yes'
+    );
+    return false;
+}
